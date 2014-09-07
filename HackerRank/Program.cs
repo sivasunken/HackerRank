2f83115ff7e8d32f57da18be48691d55ee2e089e
sivasunken <sivasankar.rs@gmail.com>
2014-09-07 15:30:50 +0530

    Defaulting Program.cs

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -6,26 +6,11 @@ using System.Threading.Tasks;
 
 namespace HackerRank
 {
-    class Solution
+    class Program
     {
         static void Main(String[] args)
         {
-            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
-            int iter, count = 0;
-            Int32.TryParse(Console.ReadLine(), out iter);
 
-            string[] rocks = new string[iter];
-            for (int i = 0; i < iter; i++)
-            {
-                rocks[i] = Console.ReadLine();
-            }
-
-            for (int i = 97; i <= 122; i++)
-            {
-                if (rocks.Where(x => x.Contains((char)i)).Count() == iter)
-                    count++;
-            }
-            Console.WriteLine(count);
         }
     }
 }
